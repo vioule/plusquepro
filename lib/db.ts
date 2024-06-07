f848b01d4cfd807378a461aa582ad76513d438e1
Vincent Cano <vcano.fr@gmail.com>
2024-06-07 07:07:19 +0000

    FIX db.ts
    - Miss save db.ts since the beggining
    - Make dbConnect working with .env info

--- lib/db.ts
+++ lib/db.ts
@@ -1,19 +1,10 @@
 import mongoose from "mongoose";
-// import { ENV_TARGET } from "./constants";
+
 declare global {
   var mongoose: any; // This must be a `var` and not a `let / const`
 }
 
 const DATABASE_URI = `mongodb://${process.env.MONGO_APP_USERNAME}:${process.env.MONGO_APP_PASSWORD}@plusquepro-mongodb-container:27017/${process.env.MONGO_DATABASE}?authSource=${process.env.MONGO_DATABASE}`;
-// const DATABASE_URI = DATABASE_URIS[ENV_TARGET];
-
-// if (!DATABASE_URI) {
-//   throw new Error(
-//     "Please define the DATABASE_URI_DEVELOPMENT, DATABASE_URI_STAGING and DATABASE_URI_PRODUCTION environment variables inside .env"
-//   );
-// }
-
-console.log(DATABASE_URI);
 
 let cached = global.mongoose;
 
